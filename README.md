e0dba9b10a225b2ef8b1b865bbcd39ccaf6483f8
Jeremy Coleman <jeremy@coglite.com>
2021-12-09 01:52:51 -0500

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# omnify
+build tools
---
+++ src/getAllProps.js
@@ -0,0 +1,8 @@
+var getAllProps = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()]
+}
